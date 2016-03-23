ca04b50753a47a39db4c35bb6ad8ea80992d38c5
Steve <sruffelman@gmail.com>
2016-03-23 13:38:26 -0700

    reformats text

--- HallOfFrame/CustomView.m
+++ HallOfFrame/CustomView.m
@@ -11,33 +11,33 @@
 @implementation CustomView
 
 - (IBAction)onCustomViewButtonTapped:(UIButton *)sender {
-    
-    [self.delegate customView:self didTapButton:sender];    
+  
+  [self.delegate customView:self didTapButton:sender];
 }
 
 - (IBAction)onSliderValueChange:(UISlider *)sender {
+  
+  if (sender.tag == 1) {
+    self.redSliderValue = sender.value;
     
-    if (sender.tag == 1) {
-        self.redSliderValue = sender.value;
-        
-    } else if (sender.tag == 2) {
-        self.blueSliderValue = sender.value;
-        
-    } else {
-        self.greenSliderValue = sender.value;
-
-    }
+  } else if (sender.tag == 2) {
+    self.blueSliderValue = sender.value;
     
+  } else {
+    self.greenSliderValue = sender.value;
+    
+  }
+  
 }
 
 - (IBAction)onSetCustomColorButtonTapped:(UIButton *)sender {
-    
-    // send custom color method to delegate as in first method above
-    [self.delegate customView:self
-      didTapCustomColorButton:sender
-                 withRedColor:self.redSliderValue
-                withBlueColor:self.blueSliderValue
-               withGreenColor:self.greenSliderValue];
+  
+  // send custom color method to delegate as in first method above
+  [self.delegate customView:self
+    didTapCustomColorButton:sender
+               withRedColor:self.redSliderValue
+              withBlueColor:self.blueSliderValue
+             withGreenColor:self.greenSliderValue];
 }
 
 @end
